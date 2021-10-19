afabec338f89705d2ccf04386c8d0be559e0d85e
Ronney Vargas <ronneyvargas@gmail.com>
2021-10-19 16:19:08 -0500

    Fixed js and added css

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
+const toggleBtn = document.getElementById("toggle-btn");
+
 const getElement = (selector) => {
     const element = document.querySelector(selector)
     if (element) return element
     throw Error(
         `Please double check your class names, there is no ${selector} class`
-    )
+        )
+}
+    
+const links = getElement('.nav-links');
+
+function toggleShowLinks() {
+    links.classList.toggle('show-links');
 }
 
-const links = getElement('.nav-link');
-const navBtnDOM = getElement('.nav-btn');
-
-navBtnDOM.addEventListener('click' ,() => {
-    links.classList.toggle('show-links')
-})
+toggleBtn.addEventListener('click', toggleShowLinks);
