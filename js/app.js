ec1895463d02831a7a5a1201b12f046d298d6f7e
Mateo <matthew27garcia@gmail.com>
2021-10-18 22:38:25 -0500

    added the javascript for the toggle button

--- js/app.js
+++ js/app.js
@@ -2,8 +2,13 @@ const getElement = (selector) => {
     const element =document.querySelector(selector)
     if (element) return element
     throw Error(
-        'Please double check your class names, there is no ${selector}class'
+        'Please double check your class names.'
     )
 }
 
 const links = getElement('.nav-link');
+const navBtnDOM = getElement('.nav-btn');
+
+navBtnDOM.addEventListener('click' ,() => {
+    links.classList.toggle('show-links')
+})
